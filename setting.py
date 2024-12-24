b3b4df795eac7f8fb8ed48ebc62611ce4c53a2ea
Coconut <43983324+Ttibo@users.noreply.github.com>
2024-12-24 09:15:21 +0100

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,23 +1,12 @@
 import os
 
 DATASET_PATHS = {
-    'SALICON': {
-        'type' : 'image',
-        'train': "/Users/coconut/Documents/Dataset/GenSaliency/VisualSaliency/SALICON/",
-        'val': "/Users/coconut/Documents/Dataset/GenSaliency/VisualSaliency/SALICON/",
+    'NAME': {
+        'type' : 'image/video',
+        'train': "path",
+        'val': "path",
         'extension' : "jpg"
     },
-    'DHF1K': {
-        'type' : 'video',
-        'path': "/Users/coconut/Documents/Dataset/GenSaliency/VisualSaliency/DHF1K/DHF1K/",
-        'extension' : "jpg",
-        'img_dir' : "images"
-    },
-    'UCFSports': {
-        'type' : 'video',
-        'path': "/Users/coconut/Documents/Dataset/GenSaliency/VisualSaliency/ucf_sports_actions/",
-        'extension' : "png",
-        'img_dir' : "images"
-    },
+
 }
 
